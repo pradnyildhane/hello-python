bc2eddb431a906ea0729a8f663e39906f43413c9
pradnyil <pradnyildhane@gmail.com>
2022-11-06 19:11:23 +0000

    changes

--- check.py
+++ check.py
@@ -1,2 +1,18 @@
 from nsepython import *
 print(indices)
+
+import numpy as np
+import pandas as pd
+
+positions = nsefetch('http://localhost:3000/nse/get_quote_info?companyName=TCS')
+df = pd.DataFrame(positions['data'])
+LTpositions = nsefetch('http://localhost:3000/nse/get_quote_info?companyName=LT')
+df1 = pd.DataFrame(LTpositions['data'])
+df.to_csv(r'/Users/pradnyilkumardhane/Downloads/dftocsv.csv', mode='a', index=False, header=False)
+#df.to_csv(r'/Users/pradnyilkumardhane/Downloads/dftocsv.csv',encoding='utf-8', index=False)
+
+# Select the ones you want
+df2 = df[['closePrice','lastPrice']]
+df2.to_csv(r'/Users/pradnyilkumardhane/Downloads/dftocsv.csv', mode='a', index=False, header=False)
+print(df2)
+
